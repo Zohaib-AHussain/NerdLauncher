b1c0ee9cc2416879152005011b07364e8bac0b9a
Zohaib-AHussain <zowaib@gmail.com>
2016-01-28 09:42:37 -0500

    Adding required imports

--- app/src/main/java/zohaibhussain/com/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/zohaibhussain/com/nerdlauncher/NerdLauncherFragment.java
@@ -1,13 +1,24 @@
 package zohaibhussain.com.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.zip.Inflater;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
